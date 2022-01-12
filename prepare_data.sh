bb6b015bc04bdfc51419fb78f775b8e21469e8c0
ih-cooker <97334101+ih-cooker@users.noreply.github.com>
2022-01-12 22:31:05 +0900

    Update prepare_data.sh

--- prepare_data.sh
+++ prepare_data.sh
@@ -1,7 +1,3 @@
-pip install scikit-learn
-pip install sentencepiece
-
-
 wget https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.dat.gz
 gunzip uniprot_sprot.dat.gz
 
